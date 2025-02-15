d2b3ca584f12e3ef8626014790c859847554a791
shazahanshakib <shazahanshakib3@gmail.com>
2025-02-15 23:41:19 +0600

    solve slide 3 problem

--- src/component/allVisas/AllVisa.jsx
+++ src/component/allVisas/AllVisa.jsx
@@ -61,7 +61,7 @@ const AllVisa = () => {
           Service we provide
         </h3>
         <h1 className="text-center text-6xl font-bold">
-          Outstanding Immigration <br /> Visa Service
+          Outstanding Immigration <br />
         </h1>
       </div>
 
